37cd921705e141f7f3dbc1307e6b165346d70fa9
Efe <isikefeoktay@gmail.com>
2021-05-07 14:24:31 +0300

    Last Sprint

---
+++ index.js
@@ -0,0 +1,65 @@
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: { lat: 40.731, lng: -73.997 },
+    });
+    const geocoder = new google.maps.Geocoder();
+    const infowindow = new google.maps.InfoWindow();
+    document.getElementById("submit").addEventListener("click", () => {
+        geocodeLatLng(geocoder, map, infowindow);
+    },
+        document.getElementById("submit2").addEventListener("click", () => {
+            geocodeAddress(geocoder, map);
+        }));
+}
+
+function geocodeLatLng(geocoder, map, infowindow) {
+    const input = document.getElementById("latlng").value;
+    const latlngStr = input.split(",", 2);
+    const latlng = {
+        lat: parseFloat(latlngStr[0]),
+        lng: parseFloat(latlngStr[1]),
+    };
+    geocoder.geocode({ location: latlng }, (results, status) => {
+        if (status === "OK") {
+            if (results[0]) {
+                map.setZoom(11);
+                const marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                });
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
+            } else {
+                window.alert("No results found");
+            }
+        } else {
+            window.alert("Geocoder failed due to: " + status);
+        }
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("address").value;
+    geocoder.geocode({ address: address }, (results, status) => {
+        if (status === "OK") {
+            resultsMap.setCenter(results[0].geometry.location);
+            new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location,
+
+            })
+
+            document.getElementById("lat").value = results[0].geometry.location.lat();
+            document.getElementById("lng").value = results[0].geometry.location.lng();
+            ;
+
+        } else {
+            alert(
+                "Geocode was not successful for the following reason: " + status
+            );
+        }
+    });
+}
+
